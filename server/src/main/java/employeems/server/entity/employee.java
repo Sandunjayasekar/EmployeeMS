2a6aaae44fe782e44247a8a8fc77a570a2d9f78f
Sandunjayasekar <jmsrjayasekara97@gmail.com>
2023-01-31 10:03:17 +0530

    Entity Added

---
+++ server/src/main/java/employeems/server/entity/employee.java
@@ -0,0 +1,21 @@
+package employeems.server.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "Employee")
+public class employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int empId;
+    private String empName;
+    private String empAddress;
+    private String empNumber;
+
+}
