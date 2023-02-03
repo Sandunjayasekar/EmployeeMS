aa203d896f3337716e42fd665d786e85fe1933e7
Sandunjayasekar <jmsrjayasekara97@gmail.com>
2023-02-03 12:40:06 +0530

    post,put and get added

--- server/src/main/java/employeems/server/service/EmployeeService.java
+++ server/src/main/java/employeems/server/service/EmployeeService.java
@@ -6,8 +6,12 @@ import employeems.server.repo.EmployeeRepo;
 import employeems.server.util.VarList;
 import jakarta.transaction.Transactional;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @Transactional
@@ -24,7 +28,20 @@ public class EmployeeService {
         }else {
             employeeRepo.save(modelMapper.map(employeeDTO, Employee.class));
             return VarList.RSP_SUCCESS;
-
         }
+    }
+    public String updateEmployee(EmployeeDTO employeeDTO){
+        if (employeeRepo.existsById(employeeDTO.getEmpId())) {
+            employeeRepo.save(modelMapper.map(employeeDTO, Employee.class));
+            return VarList.RSP_SUCCESS;
+
+        }else {
+            return VarList.RSP_NO_DATA_FOUND;
+        }
+    }
+    public List<EmployeeDTO> getAllEmployee(){
+        List<Employee> employeeList = employeeRepo.findAll();
+        return modelMapper.map(employeeList,new TypeToken<ArrayList<EmployeeDTO>>(){
+        }.getType());
     }
 }
